96143f45f02f1b8134edd76c856571c188c101a2
Dennis Bär <dennis.baer@student.reutlingen-university.de>
2023-12-02 20:53:17 +0100

    Refactor Link cards in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,60 @@
 import Link from "next/link";
 import { FaUser, FaSignInAlt, FaUserPlus, FaRunning } from "react-icons/fa";
 
+interface LinkCardProps {
+	href: string;
+	title: string;
+	description: string;
+	Icon: React.ElementType;
+}
+
+const LinkCard: React.FC<LinkCardProps> = ({
+	href,
+	title,
+	description,
+	Icon,
+}) => (
+	<Link href={href}>
+		<div className="border border-gray-300 p-4 rounded shadow hover:shadow-lg transition-all duration-200 flex items-center justify-between hover:border-blue-500">
+			<div>
+				<h2>{title}</h2>
+				<p className="text-gray-500">{description}</p>
+			</div>
+			<Icon />
+		</div>
+	</Link>
+);
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col  justify-between p-24">
 			<div>
 				<h1>Home Page</h1>
 				<div className="mt-4 flex flex-col gap-4 w-full md:w-1/2">
-					<Link href="/login">
-						<div className="border border-gray-300 p-4 rounded shadow hover:shadow-lg transition-shadow duration-200 flex items-center justify-between">
-							<div>
-								<h2>Login</h2>
-								<p className="text-gray-500">Go to the login page</p>
-							</div>
-							<FaSignInAlt />
-						</div>
-					</Link>
-					<Link href="/signup">
-						<div className="border border-gray-300 p-4 rounded shadow hover:shadow-lg transition-shadow duration-200 flex items-center justify-between">
-							<div>
-								<h2>Sign Up</h2>
-								<p className="text-gray-500">Go to the sign up page</p>
-							</div>
-							<FaUserPlus />
-						</div>
-					</Link>
-
-					<Link href="/account">
-						<div className="border border-gray-300 p-4 rounded shadow hover:shadow-lg transition-shadow duration-200 flex items-center justify-between">
-							<div>
-								<h2>Account</h2>
-								<p className="text-gray-500">Go to the account page</p>
-							</div>
-							<FaUser />
-						</div>
-					</Link>
-					<Link href="/activity/200">
-						<div className="border border-gray-300 p-4 rounded shadow hover:shadow-lg transition-shadow duration-200 flex items-center justify-between">
-							<div>
-								<h2>Activity</h2>
-								<p className="text-gray-500">Go to the activity page</p>
-							</div>
-							<FaRunning />
-						</div>
-					</Link>
+					<LinkCard
+						href="/login"
+						title="Login"
+						description="Go to the login page"
+						Icon={FaSignInAlt}
+					/>
+					<LinkCard
+						href="/signup"
+						title="Sign Up"
+						description="Go to the sign up page"
+						Icon={FaUserPlus}
+					/>
+					<LinkCard
+						href="/account"
+						title="Account"
+						description="Go to the account page"
+						Icon={FaUser}
+					/>
+					<LinkCard
+						href="/activity/200"
+						title="Activity"
+						description="Go to the activity page"
+						Icon={FaRunning}
+					/>
 				</div>
 			</div>
 		</main>
